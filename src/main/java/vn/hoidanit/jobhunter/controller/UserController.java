6dbb1106c1403940dd131bafc0f9efbc80397167
Ductran-05 <duc30092005@gmail.com>
2025-03-12 22:25:54 +0700

    CRUD co ban

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,6 +1,9 @@
 package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +12,8 @@ import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
@@ -31,4 +36,21 @@ public class UserController {
     public void deleteUser(@PathVariable("id") long id) {
         this.userService.handleDeleteUser(id);
     }
+
+    @GetMapping("/user/{id}")
+    public User getUserByID(@PathVariable("id") long id) {
+        return this.userService.getUserByID(id);
+        // return "get user";
+    }
+
+    @GetMapping("/user")
+    public List<User> findAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody User user) {
+        this.userService.updateUser(user);
+    }
+
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.User;
@@ -19,5 +21,28 @@ public class UserService {
 
     public void handleDeleteUser(long id) {
         this.userRepository.deleteById(id);
+    }
+
+    public User getUserByID(long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        try {
+            return this.userRepository.findAll();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return null;
+    }
+
+    public void updateUser(User user) {
+
+        this.userRepository.findById(user.getId());
+        if (this.userRepository.findById(user.getId()).isPresent()) {
+            this.userRepository.save(user);
+        } else {
+            throw new RuntimeException("User not found for ID :: " + user.getId());
+        }
     }
 }
